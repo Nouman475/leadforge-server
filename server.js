194959ca4bfad8341e9598331624831e0a26b4f8
Muhammad Nouman <140593593+Nouman475@users.noreply.github.com>
2025-08-31 13:59:42 +0500

    Update server.js

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(helmet());
 // app.use(morgan('combined'));
 app.use(limiter);
 app.use(cors({
-  origin: 'https://leadforge-n.netlify.app/',
+  origin: 'https://leadforge-n.netlify.app',
   credentials: true
 }));
 app.use(express.json({ limit: '10mb' }));
